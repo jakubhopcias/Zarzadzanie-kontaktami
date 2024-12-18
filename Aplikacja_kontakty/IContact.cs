7959024b7846190e8f35b494aacb357a8fb3c54e
Wujas <hopciasjakub1@gmail.com>
2024-12-18 14:57:35 +0100

    Dodadno interfejs IContact

---
+++ Aplikacja_kontakty/IContact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja_kontakty
+{
+    public interface IContact
+    {
+        int Id { get; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string PhoneNumber { get; set; }
+        string Email { get; set; }
+        DateTime CreatedAt { get; }
+        DateTime UpdatedAt { get; set; }
+    }
+}
