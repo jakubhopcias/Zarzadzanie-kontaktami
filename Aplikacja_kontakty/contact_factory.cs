2f2d298a0b9fb4c0d548799b5cf6f0477bffbcd5
Jakub <habdasjakub@gmail.com>
2024-12-20 12:40:47 +0100

    Utworzenie klasy contact_factory

---
+++ Aplikacja_kontakty/contact_factory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja_kontakty
+{
+    public static class ContactFactory
+    {
+        private static int nextId = 1;
+
+        public static IContact CreateContact(string firstName, string lastName, string phoneNumber, string email)
+        {
+            return new Contact(nextId++, firstName, lastName, phoneNumber, email);
+        }
+    }
+}
