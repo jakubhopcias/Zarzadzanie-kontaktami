0b7bf2b4a1742618deaeb3901275d41fd36cc5cb
kaduucek <paulinakad@wp.pl>
2024-12-20 12:42:09 +0100

    Update 2 Program.cs
    Dodanie wstępnego menu.

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -7,7 +7,21 @@ namespace Aplikacja_kontakty
         static void Main(string[] args)
         {
             ContactManager contactManager = new ContactManager();
+             while (true)
+            {
+                Console.WriteLine("\n--- Aplikacja do Zarządzania Kontaktami ---");
+                Console.WriteLine("1. Dodaj kontakt");
+                Console.WriteLine("2. Edytuj kontakt");
+                Console.WriteLine("3. Usuń kontakt");
+                Console.WriteLine("4. Wyszukaj kontakt");
+                Console.WriteLine("5. Wyświetl wszystkie kontakty");
+                Console.WriteLine("6. Wyjście");
+                Console.Write("Wybierz opcję: ");
 
+                string option = Console.ReadLine();
+
+                
+             }
         }
     }
 }
