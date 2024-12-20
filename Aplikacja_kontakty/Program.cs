f89a42c315b099d422f82f16dc6a771e902cfe18
kaduucek <paulinakad@wp.pl>
2024-12-20 12:47:40 +0100

    2. Update Program.cs
    Utworzenie podstaw menu.

--- Aplikacja_kontakty/Program.cs
+++ Aplikacja_kontakty/Program.cs
@@ -6,7 +6,16 @@ namespace Aplikacja_kontakty
         {
             ContactManager contactManager = new ContactManager();
 
-          
+            while (true)
+            {
+                Console.WriteLine("\n--- Aplikacja do Zarządzania Kontaktami ---");
+                Console.WriteLine("1. Dodaj kontakt");
+                Console.WriteLine("6. Wyjście");
+                Console.Write("Wybierz opcję: ");
+
+                string option = Console.ReadLine();
+                
+            }
         }
     }
 }
